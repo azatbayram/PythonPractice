ca6c6f1230dbd9ba91b800d6b55b6cf31d76c386
azatbayram <bayramov.azat.tkm@gmail.com>
2023-01-04 00:30:41 +0300

    add if elif else statement

---
+++ Python/day7ControlFlow/IfElifStatement.py
@@ -0,0 +1,22 @@
+#if elif else
+# program to display week days with if else if
+
+day = 3
+
+if(day==1):
+    print("Monday")
+elif(day==2):
+    print("Tuesday")
+elif(day==3):
+    print("Wednesday")
+elif(day==4):
+    print("Thursday")
+elif(day==5):
+    print("Friday")
+elif(day==6):
+    print("Saturday")
+elif(day==7):
+    print("Sunday")
+else:
+    print("not valid day")
+
